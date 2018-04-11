9557c5838d85a1c808e77ad29f8ac2399356da20
rungokarol <karolrungo@gmail.com>
2018-04-11 19:52:04 +0200

    request for fetching list of recipies

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,14 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module! Variable X=${x}`)
+import axios from 'axios';
+
+async function getResults(query) {
+    const proxy = `https://cors-anywhere.herokuapp.com/`;
+    const key = `3708c059aa5e73f41d1895174c25f13e`;
+    try{
+        const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`)
+    console.log(res.data.recipes)
+    } catch(error){
+        alert(error)
+    }
+}
+
+getResults('pizza');
