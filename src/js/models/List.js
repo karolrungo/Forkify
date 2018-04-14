561f35b382f18b4a3351ecde7cb04b730c0427df
rungokarol <karolrungo@gmail.com>
2018-04-14 07:14:43 +0200

    shopping list model

---
+++ src/js/models/List.js
@@ -0,0 +1,28 @@
+import uniqid from 'uniqid'
+
+export default class List{
+    constructor(){
+        this.items = []
+    }
+
+    additem (count, unit, ingredient){
+        const item = {
+            id: uniqid(),
+            count,
+            unit,
+            ingredient
+        }
+        this.items.push(item)
+    }
+
+    deleteitem (id){
+        const index = this.items.findIndex( el => el.id === id)
+        if (index > -1){
+            this.items.splice(index, 1);
+        }
+    }
+
+    updateCount(id, newCount){
+        this.items.find(el => el.id === id).count = newCount
+    }
+}
